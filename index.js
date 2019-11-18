9f01771b82b5de461bd153a77489510faf9e8f00
LaelLuo <laelluo@qq.com>
2019-11-18 09:59:14 +0800

    feat:支持生成dart文件

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const
     args = utils.getArgs(process),
     src = path.resolve(args.src),
     dist = path.resolve(args.dist),
-    reg = /r['"](.*)['"]/g,
+    catchRregexp = /r['"](.*)['"]/g,
     replace = 'AppLocalizations.of(context).',
     importString = 'import \'package:opf_business/generated/app_localizations.dart\';\n';
 
@@ -17,11 +17,11 @@ function saveArb() {
     utils.walk(src, (filePath, data) => {
         print(filePath)
         let result, lastIndex
-        if (reg.test(data)) { data = importString + data }
-        reg.lastIndex = 0
+        if (catchRregexp.test(data)) { data = importString + data }
+        catchRregexp.lastIndex = 0
         while (lastIndex != 0) {
-            result = reg.exec(data)
-            lastIndex = reg.lastIndex
+            result = catchRregexp.exec(data)
+            lastIndex = catchRregexp.lastIndex
             if (result != null) {
                 utils.analyze(result[1], (analyzeResult) => {
                     while (arb[analyzeResult.key] != null &&
@@ -39,10 +39,63 @@ function saveArb() {
         }
         utils.save(filePath, data)
     })
-    utils.save(path.join(dist, 'arb', `zh_${Date.now()}.arb`), JSON.stringify(arb, null, '\t'))
+    utils.save(path.join(dist, 'arb', `zh_${Date.now()}.arb`), JSON.stringify(arb, null, ''))
 }
 
 (() => {
-    let temp = fs.readFileSync('./data/app_localizations.dart.temp').toString()
-    saveArb()
+    let arb = {}
+    let localeString = ""
+    let loadString = ""
+    let classString = ""
+    let getString = ""
+    let dirParh = path.join(dist, 'arb')
+    let paramsRegexp = /\$\{([^\}]*)\}/
+    fs.readdirSync(dirParh).forEach((fileName) => {
+        arb[fileName.substr(0, 2)] = JSON.parse(fs.readFileSync(path.join(dirParh, fileName)).toString())
+    })
+    Object.keys(arb).forEach((langCode) => {
+        localeString += `\nLocale("${langCode}", ""),\n`
+        loadString += `case "${langCode}":\nAppLocalizations.current = const $${langCode}();\nreturn SynchronousFuture<AppLocalizations>(AppLocalizations.current);`
+        if (langCode == 'zh') {
+            let zh = arb[langCode];
+            Object.keys(zh).forEach((key) => {
+                let value = zh[key]
+                if (/\$\{.*\}/.test(value)) {
+                    let lastIndex, params = []
+                    while (lastIndex != 0) {
+                        params.push(`String ${paramsRegexp.exec(value)[1]}`)
+                        lastIndex = paramsRegexp.lastIndex
+                    }
+                    getString += `\n  String ${key}(${params.join(',')}) => "${value}";\n`
+                } else {
+                    getString += `\n  String get ${key} => "${value}";\n`
+                }
+            })
+            classString += `class $${langCode} extends AppLocalizations {\nconst $${langCode}();\n}\n`
+        } else {
+            let overrideString = ""
+            let lang = arb[langCode];
+            Object.keys(lang).forEach((key) => {
+                let value = lang[key]
+                if (/\$\{.*\}/.test(value)) {
+                    let lastIndex, params = []
+                    while (lastIndex != 0) {
+                        params.push(`String ${paramsRegexp.exec(value)[1]}`)
+                        lastIndex = paramsRegexp.lastIndex
+                    }
+                    overrideString += `\n@override\n  String ${key}(${params.join(',')}) => "${value}";\n`
+                } else {
+                    overrideString += `\n@override\n  String get ${key} => "${value}";\n`
+                }
+            })
+            classString += `class $${langCode} extends AppLocalizations {\nconst $${langCode}();\n${overrideString}}\n`
+        }
+    })
+    let temp = fs.readFileSync('./data/app_localizations.dart.temp')
+        .toString()
+        .replace('@locale', localeString)
+        .replace('@load', loadString)
+        .replace('@class', classString)
+        .replace('@get', getString)
+    utils.save('./data/app_localizations.dart', temp)
 })()
